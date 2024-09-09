148ee25785bcf82346cf114eea85ef22fef042d7
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-09-09 12:39:37 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,5 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("ABXY", soundex);
-  EXPECT_STREQ(soundex,"A12Y");
+  EXPECT_STREQ(soundex,"A120");
 }
