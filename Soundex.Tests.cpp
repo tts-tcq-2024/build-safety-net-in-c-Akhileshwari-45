9c0f4b741b59f70c2555584d417e8eb7f38312a3
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-09-09 12:44:11 +0530

    Unit test Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,27 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
+  generateSoundex("AB", soundex);
+  EXPECT_STREQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
   generateSoundex("ABXY", soundex);
   EXPECT_STREQ(soundex,"A120");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
+  generateSoundex("CDLM", soundex);
+  EXPECT_STREQ(soundex,"C345");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
+  generateSoundex("PJAR", soundex);
+  EXPECT_STREQ(soundex,"P206");
 }
